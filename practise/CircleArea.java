ca010fc2d8c6c00c60c6515b0e5c187784249780
Soumyojyotisaha <soumyojyotisaha2001offic@gmail.com>
2024-08-02 02:11:45 +0530

    codes

--- practise/CircleArea.java
+++ practise/CircleArea.java
@@ -8,5 +8,6 @@ public class CircleArea {
         scanner.close();
         double area = Math.PI * radius * radius;
         System.out.println("The area of the circle is: " + area);
+        System.out.println("The value of pie is "+ Math.PI);
     }
 }
--- practise/NumberPattern2.java
+++ practise/NumberPattern2.java
@@ -2,12 +2,11 @@ public class NumberPattern2 {
     public static void main(String[] args) {
         int totalRows = 4; // Total number of rows in the pattern
 
-        for (int i = 0; i < totalRows; i++) {
+        for (int i = 0; i <totalRows; i++) {
             // Print numbers
-            for (int j = 1; j <= totalRows - i; j++) {
+            for (int j = 1; j <= totalRows-i; j++) {
                 System.out.print(j);
             }
-            
             // Move to the next line
             System.out.println();
         }
